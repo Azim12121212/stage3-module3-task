6b0e03067363081112a525043880d5c602cbb528
Azimbek <omar.azimbek@gmail.com>
2024-03-15 17:06:45 +0600

    3.3.10 Fixed main class

--- module-main/src/main/java/com/mjc/school/main/Main.java
+++ module-main/src/main/java/com/mjc/school/main/Main.java
@@ -4,11 +4,12 @@ import com.mjc.school.controller.menu.AppMenuController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Main.class);
+        ConfigurableApplicationContext ctx = SpringApplication.run(Main.class);
         AppMenuController app = ctx.getBean(AppMenuController.class);
         app.runApp();
     }
